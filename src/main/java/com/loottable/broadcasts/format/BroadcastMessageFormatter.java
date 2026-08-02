8eac34297f0635182af93c18c67dbef1b45b61ac
Nicholas Mckendall <nicholas.mckendall@gmail.com>
2026-08-01 22:23:19 -0400

    Add XP milestone broadcast formatting

--- src/main/java/com/loottable/broadcasts/format/BroadcastMessageFormatter.java
+++ src/main/java/com/loottable/broadcasts/format/BroadcastMessageFormatter.java
@@ -2,16 +2,23 @@ package com.loottable.broadcasts.format;
 
 import com.loottable.broadcasts.model.BroadcastEvent;
 import com.loottable.broadcasts.model.BroadcastEventType;
+import com.loottable.broadcasts.model.XpMilestoneEvent;
 
 public class BroadcastMessageFormatter
 {
     public String format(BroadcastEvent event)
-    {
-        if (event == null)
-        {
-            throw new IllegalArgumentException("event must not be null");
-        }
+	{
+		if (event == null)
+		{
+			throw new IllegalArgumentException("event must not be null");
+		}
 
-        throw new UnsupportedOperationException("Broadcast message formatting is not implemented yet");
-    }
+		if (event.getEventType() == BroadcastEventType.XP_MILESTONE)
+		{
+			XpMilestoneEvent xpEvent = (XpMilestoneEvent) event;
+			return "News: " + xpEvent.getPlayerName() + " has achieved level " + xpEvent.getLevel() + " in " + xpEvent.getSkill().getDisplayName() + ".";
+		}
+
+		throw new UnsupportedOperationException("Broadcast message formatting is not implemented yet");
+	}
 }
